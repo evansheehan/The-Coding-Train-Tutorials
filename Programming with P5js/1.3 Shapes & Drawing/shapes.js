d4a4e8396f49eb7b97b285050bf67cf1ed815c2f
awubis <awubis@gmail.com>
2019-08-28 16:59:59 -0500

    Implemented basic random circle pattern on canvas

--- Programming with P5js/1.3 Shapes & Drawing/shapes.js
+++ Programming with P5js/1.3 Shapes & Drawing/shapes.js
@@ -1,9 +1,27 @@
-//Nothing yet
+var randParam1;
+var randParam2;
+var randParam3;
+var scale;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
 }
+
 function draw() {
     background(220, 0, 200);
+    for (var i = 100; i > 0; i--) {
+        scale = i*2;
+        generateRands();
+        circle(randParam1, randParam2, randParam3);
+    }
+    noloop();
+}
 
-    rect(0, 0, 300, 300);
+function generateRands() {
+    randParam1 = Math.random()*scale;
+    //document.write(randParam1, "\n");
+    randParam2 = Math.random()*scale;
+    //document.write(randParam2, "\n");
+    randParam3 = Math.random()*scale;
+    //document.write(randParam3, "\n");
 }
